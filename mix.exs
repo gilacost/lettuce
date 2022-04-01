38ef520bfa2eb923ba29c9386b050e443acec37a
Josep Lluis Giralt D'Lacoste <josepgiraltdlacoste@gmail.com>
2022-04-01 11:33:34 +0100

    Update to elixir 1.13.2

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Lettuce.MixProject do
     [
       app: :lettuce,
       version: "0.1.5",
-      elixir: "~> 1.10.2",
+      elixir: "~> 1.13.2",
       start_permanent: Mix.env() == :prod,
       build_embedded: Mix.env() == :prod,
       deps: deps(),
--- test/fixtures/compiler_opts/config/config.exs
+++ test/fixtures/compiler_opts/config/config.exs
@@ -1,4 +1,4 @@
-use Mix.Config
+import Config
 
 config :lettuce,
   folders_to_watch: ["lib"],
